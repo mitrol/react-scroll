5d44234e2b55f5272ea7f8e481306466c436fd76
nicosampler <nf.dominguez.87@gmail.com>
2018-08-06 15:57:35 -0300

    format

--- modules/mixins/scroller.js
+++ modules/mixins/scroller.js
@@ -1,12 +1,11 @@
-import utils  from './utils';
-import animateScroll from './animate-scroll';
-import events from'./scroll-events';
+import utils from "./utils";
+import animateScroll from "./animate-scroll";
+import events from "./scroll-events";
 
-let __mapped = {}
+let __mapped = {};
 let __activeLink;
 
 export default {
-
   unmount: () => {
     __mapped = {};
   },
@@ -15,68 +14,72 @@ export default {
     __mapped[name] = element;
   },
 
-  unregister: (name) => {
+  unregister: name => {
     delete __mapped[name];
   },
 
-  get: (name) => __mapped[name] || document.getElementById(name) || document.getElementsByName(name)[0] || document.getElementsByClassName(name)[0],
+  get: name =>
+    __mapped[name] ||
+    document.getElementById(name) ||
+    document.getElementsByName(name)[0] ||
+    document.getElementsByClassName(name)[0],
 
-  setActiveLink: (link) => __activeLink = link,
+  setActiveLink: link => (__activeLink = link),
 
   getActiveLink: () => __activeLink,
 
   scrollTo(to, props) {
+    let target = this.get(to);
 
-      let target = this.get(to);
+    if (!target) {
+      console.warn("target Element not found");
+      return;
+    }
 
-      if(!target) {
-        console.warn("target Element not found");
-        return;
-      }
+    props = Object.assign({}, props, { absolute: false });
 
-      props = Object.assign({}, props, { absolute : false });
+    let containerId = props.containerId;
+    let container = props.container;
 
-      let containerId = props.containerId;
-      let container = props.container;
+    let containerElement;
+    if (containerId) {
+      containerElement = document.getElementById(containerId);
+    } else if (container && container.nodeType) {
+      containerElement = container;
+    } else {
+      containerElement = document;
+    }
 
-      let containerElement;
-      if(containerId) {
-        containerElement = document.getElementById(containerId);
-      } else if(container && container.nodeType) {
-        containerElement = container;
-      } else {
-        containerElement = document;
-      }
+    if (events.registered.begin) {
+      events.registered.begin(to, target);
+    }
 
-      if(events.registered.begin) {
-        events.registered.begin(to, target);
-      }
+    props.absolute = true;
 
-      props.absolute = true;
+    let scrollOffset =
+      utils.scrollOffset(containerElement, target) + (props.offset || 0);
 
-      let scrollOffset = utils.scrollOffset(containerElement, target) + (props.offset || 0);
-
-      /*
+    /*
        * if animate is not provided just scroll into the view
        */
-      if(!props.smooth) {
-        if (containerElement === document) {
-          window.scrollTo(0, scrollOffset);
-        } else {
-          containerElement.scrollTop = scrollOffset;
-        }
-
-        if(events.registered['end']) {
-          events.registered['end'](to, target);
-        }
-
-        return;
+    if (!props.smooth) {
+      if (containerElement === document) {
+        window.scrollTo(0, scrollOffset);
+      } else {
+        containerElement.scrollTop = scrollOffset;
       }
 
-      /*
+      if (events.registered["end"]) {
+        events.registered["end"](to, target);
+      }
+
+      return;
+    }
+
+    /*
        * Animate scrolling
        */
 
-      animateScroll.animateTopScroll(scrollOffset, props, to, target);
+    animateScroll.animateTopScroll(scrollOffset, props, to, target);
   }
 };
--- modules/mixins/utils.js
+++ modules/mixins/utils.js
@@ -1,28 +1,36 @@
-const pushHash = (hash) => {
-  hash = hash
-    ? hash.indexOf('#') === 0
-      ? hash
-      : '#' + hash
-    : '';
+const pushHash = hash => {
+  hash = hash ? (hash.indexOf("#") === 0 ? hash : "#" + hash) : "";
 
-  if(history.pushState) {
+  if (history.pushState) {
     let loc = window.location;
-    history.pushState(null, null, hash ? loc.pathname + loc.search + hash
-      // remove hash
-      : loc.pathname + loc.search);
+    history.pushState(
+      null,
+      null,
+      hash
+        ? loc.pathname + loc.search + hash
+        : // remove hash
+          loc.pathname + loc.search
+    );
   } else {
     location.hash = hash;
   }
-}
+};
 
-const  getHash = () => {
-  return window.location.hash.replace(/^#/, '');
-}
+const getHash = () => {
+  return window.location.hash.replace(/^#/, "");
+};
 
-const filterElementInContainer = (container) => (element) => container.contains ? container != element && container.contains(element) : !!(container.compareDocumentPosition(element) & 16)
+const filterElementInContainer = container => element =>
+  container.contains
+    ? container != element && container.contains(element)
+    : !!(container.compareDocumentPosition(element) & 16);
 
-const scrollOffset = (c, t) => c === document ? 
-      t.getBoundingClientRect().top + (window.scrollY || window.pageYOffset) : getComputedStyle(c).position === "relative" ? t.offsetTop : (t.getBoundingClientRect().top + c.scrollTop)
+const scrollOffset = (c, t) =>
+  c === document
+    ? t.getBoundingClientRect().top + (window.scrollY || window.pageYOffset)
+    : getComputedStyle(c).position === "relative"
+      ? t.offsetTop
+      : t.getBoundingClientRect().top + c.scrollTop;
 
 export default {
   pushHash,
